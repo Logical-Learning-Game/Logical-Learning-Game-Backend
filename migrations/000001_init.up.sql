d734bb4bd9795923c4dfb9e7dfe116af0b5919a9
chuckboliver <patpumhak@gmail.com>
2022-09-19 20:44:17 +0700

    :heavy_plus_sign: feat(database): Create new migration
    - player table
    - player login log table

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE player (
+    player_id VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY(player_id)
+);
+
+CREATE TABLE login_log (
+    player_id VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    FOREIGN KEY(player_id) REFERENCES player(player_id)
+);
