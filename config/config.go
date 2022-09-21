bea74dfd4056ae39e68f77eaebc1c5f430615387
chuckboliver <patpumhak@gmail.com>
2022-09-21 18:02:57 +0700

    :heavy_plus_sign: feat(config): Add config package
    - read configuration from `app.env` configuration file
    - update `.gitignore` to ignore secret in configuration file
    - add `app.env.example` for other contributor

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	HTTP    `mapstructure:",squash"`
+	MariaDB `mapstructure:",squash"`
+}
+
+type HTTP struct {
+	Port string `mapstructure:"SERVER_PORT"`
+}
+
+type MariaDB struct {
+	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	var config Config
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	err := viper.Unmarshal(&config)
+
+	return &config, err
+}
