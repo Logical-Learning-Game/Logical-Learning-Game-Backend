c71b0a54a1d488e697c486c779e20e99ea241bc5
chuckboliver <patpumhak@gmail.com>
2022-09-23 01:09:22 +0700

    :green_heart: refactor(database): Add logging to `New` function

--- pkg/mariadb/mariadb.go
+++ pkg/mariadb/mariadb.go
@@ -2,12 +2,18 @@ package mariadb
 
 import (
 	"database/sql"
+	"llg_backend/pkg/logger"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func New(url string) (*sql.DB, error) {
+	logger.GlobalLog.Info("connecting to mariadb...")
 	conn, err := sql.Open("mysql", url)
+	if err != nil {
+		logger.GlobalLog.Debugw("connect to mariadb failed", "err", err)
+	}
 
+	logger.GlobalLog.Info("connect to mariadb successful")
 	return conn, err
 }
