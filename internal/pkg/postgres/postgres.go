356bbaa57bc4c3a334c7ea0292c63cbcf0ea25c8
chuckboliver <patpumhak@gmail.com>
2023-03-07 02:44:52 +0700

    :sparkles: feat: Split out postgres config from single uri field to multiple field

--- internal/pkg/postgres/postgres.go
+++ internal/pkg/postgres/postgres.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"fmt"
 	"llg_backend/pkg/logger"
 
 	"gorm.io/driver/postgres"
@@ -8,14 +9,22 @@ import (
 )
 
 type Config struct {
-	URI   string `mapstructure:"POSTGRES_URI"`
-	Debug bool   `mapstructure:"GORM_DEBUG"`
+	Host     string `mapstructure:"POSTGRES_HOST"`
+	Port     string `mapstructure:"POSTGRES_PORT"`
+	User     string `mapstructure:"POSTGRES_USER"`
+	Password string `mapstructure:"POSTGRES_PASSWORD"`
+	DBName   string `mapstructure:"POSTGRES_DB"`
+	SSLMode  string `mapstructure:"POSTGRES_SSL_MODE"`
+	Debug    bool   `mapstructure:"GORM_DEBUG"`
 }
 
 func New(config *Config) (*gorm.DB, error) {
-	logger.GlobalLog.Infof("connecting to postgres at %s", config.URI)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		config.Host, config.User, config.Password, config.DBName, config.Port, config.SSLMode)
 
-	db, err := gorm.Open(postgres.Open(config.URI), &gorm.Config{})
+	logger.GlobalLog.Infof("connecting to postgres at %s", dsn)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		logger.GlobalLog.Errorw("connect to postgres failed", "err", err)
 		return nil, err
